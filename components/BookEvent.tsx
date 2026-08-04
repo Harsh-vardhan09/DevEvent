63e93c3432131630d7ae987ea4b621eb39193eda
Harsh <harsh.vardhanp0901@gmail.com>
2026-08-05 02:28:06 +0530

    applied api routes

---
+++ components/BookEvent.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react";
+
+
+const BookEvent = () => {
+  const [email, setEmail] = useState("");
+  const [submitted, setsubmitted] = useState(false);
+
+
+  return <div>Book Event</div>;
+};
+
+export default BookEvent;
